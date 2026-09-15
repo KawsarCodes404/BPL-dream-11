a672c2c30beeb2b95dd6df07355127fe7cc0b1bb
Ridwanulhaquekawsar <kawsarhaque430@gmail.com>
2026-09-16 01:55:23 +0600

    Now, It is midnight

--- src/components/players/Players.tsx
+++ src/components/players/Players.tsx
@@ -3,30 +3,48 @@ import type { Iplayer } from "../../types/player";
 import AvailablePlayers from "./AvailablePlayers";
 
 interface PlayersProps {
-    playersPromise : Promise<Iplayer[]>;
+    playersPromise: Promise<Iplayer[]>;
 }
 
-const Players = ({playersPromise} : PlayersProps) => {
+const Players = ({ playersPromise }: PlayersProps) => {
 
     const players = use(playersPromise);
 
     const [buttonType, setbuttonType] = useState("Available");  // It holds Data, just like a variable does !  [state , set state]  !
 
     // console.log(test);
+    const handleUpdateBtnType = (type : "Available" | "Selected") => {
+        setbuttonType(type)
+    }
 
     return (
         <div className="container mx-auto">
-            
+            {/* Heading Text */}
             <div className="flex justify-between gap-4 mb-2">
+                {/* left text */}
                 <h2 className="font-bold text-xl">Available Players</h2>
 
+                {/* Right Text  */}
                 <div>
-                    <button className={`btn btn-success rounded-r-none`}>Available</button>
-                    <button className="btn rounded-l-none">Selected</button>
+                    <button
+                        onClick={() => handleUpdateBtnType("Available")}
+                        className={`btn ${buttonType === "Available" ? "btn-success" : ""} rounded-r-none`}
+                    >
+                        Available
+                    </button>
+
+                    <button
+                        onClick={() => handleUpdateBtnType("Selected")}
+                        className={`btn ${buttonType === "Selected" ? "btn-success" : ""} rounded-l-none`}
+                    >
+                        Selected
+                    </button>
                 </div>
             </div>
 
-            <AvailablePlayers players = {players} />
+
+            {/* Players card section */}
+            <AvailablePlayers players={players} />
         </div>
     );
 };
