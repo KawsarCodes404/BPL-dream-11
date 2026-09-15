9a9d14ff44e9ecb9226305292361e597c8123071
Ridwanulhaquekawsar <kawsarhaque430@gmail.com>
2026-09-16 02:48:47 +0600

    now just go to the github safely

--- src/components/players/Players.tsx
+++ src/components/players/Players.tsx
@@ -1,6 +1,7 @@
 import { use, useState } from "react";
 import type { Iplayer } from "../../types/player";
 import AvailablePlayers from "./AvailablePlayers";
+import SelectedPlayers from "./SelectedPlayers";
 
 interface PlayersProps {
     playersPromise: Promise<Iplayer[]>;
@@ -13,28 +14,28 @@ const Players = ({ playersPromise }: PlayersProps) => {
     const [buttonType, setbuttonType] = useState("Available");  // It holds Data, just like a variable does !  [state , set state]  !
 
     // console.log(test);
-    const handleUpdateBtnType = (type : "Available" | "Selected") => {
-        setbuttonType(type)
-    }
+    // const handleUpdateBtnType = (type : "Available" | "Selected") => {
+    //     setbuttonType(type)
+    // }
 
     return (
         <div className="container mx-auto">
             {/* Heading Text */}
             <div className="flex justify-between gap-4 mb-2">
                 {/* left text */}
-                <h2 className="font-bold text-xl">Available Players</h2>
+                <h2 className="font-bold text-xl">{buttonType === "Available" ? "Available Players" : "Selected Players"}</h2>
 
                 {/* Right Text  */}
                 <div>
                     <button
-                        onClick={() => handleUpdateBtnType("Available")}
+                        onClick={() => setbuttonType("Available")}
                         className={`btn ${buttonType === "Available" ? "btn-success" : ""} rounded-r-none`}
                     >
                         Available
                     </button>
 
                     <button
-                        onClick={() => handleUpdateBtnType("Selected")}
+                        onClick={() => setbuttonType("Selected")}
                         className={`btn ${buttonType === "Selected" ? "btn-success" : ""} rounded-l-none`}
                     >
                         Selected
@@ -44,7 +45,7 @@ const Players = ({ playersPromise }: PlayersProps) => {
 
 
             {/* Players card section */}
-            <AvailablePlayers players={players} />
+            {buttonType === "Available" ? <AvailablePlayers players={players} /> : <SelectedPlayers />}
         </div>
     );
 };
